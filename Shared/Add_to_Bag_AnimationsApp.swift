ecf63275fc17d3e9e679a0ad270abc80fe3e56c8
yuan <dnstylish@gmail.com>
2021-10-22 14:04:34 +0700

    Initial Commit

---
+++ Shared/Add_to_Bag_AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Add_to_Bag_AnimationsApp.swift
+//  Shared
+//
+//  Created by Yuan on 22/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct Add_to_Bag_AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
